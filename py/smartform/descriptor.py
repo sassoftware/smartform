1876fa01b13895c9cd8d9655ea5bcef246ca13ee
Elliot Peele <https://issues.rpath.com/>
2012-07-17 16:08:59 -0400

    make to store descriptions and description objects rather than strings

--- py/smartform/descriptor.py
+++ py/smartform/descriptor.py
@@ -340,18 +340,17 @@ class BaseDescriptor(_BaseClass):
         if values is None:
             return None
         if isinstance(values, dict):
-            langMap = values
-        else:
-            if not isinstance(values, list):
-                values = [ values ]
-            # Eliminate duplicates
-            langMap = {}
-            for val in values:
-                if isinstance(val, (str, unicode)):
-                    val = self.Description(val)
-                elif isinstance(val, tuple):
-                    val = self.Description(val[0], val[1])
-                langMap[val.get_lang()] = val
+            values = values.items()
+        if not isinstance(values, list):
+            values = [ values ]
+        # Eliminate duplicates
+        langMap = {}
+        for val in values:
+            if isinstance(val, (str, unicode)):
+                val = self.Description(val)
+            elif isinstance(val, tuple):
+                val = self.Description(val[0], val[1])
+            langMap[val.get_lang()] = val
         ret = self.xmlFactory().descriptionsTypeSub.factory()
         ret.set_desc([ x[1] for x in sorted(langMap.items()) ])
         return ret
